42543a487f05a4e35ed967ff76a4db6ca22561d7
AgueroZZ <49107284+AgueroZZ@users.noreply.github.com>
2019-11-25 18:57:26 -0500

    Add files via upload

---
+++ Simluation_RW2/00-load-packages.R
@@ -0,0 +1,24 @@
+# LOAD LIBRARIES FOR CASE-CROSSOVER DATA ANALYSIS ###
+# ALSO INCLUDE HELPER FUNCTIONS
+
+# Packages required for computation
+suppressWarnings(suppressMessages(require(ipoptr)))
+library(parallel)
+library(Matrix)
+library(trustOptim)
+library(matrixStats) # for LogSumExp
+library(purrr)
+library(rstanarm)
+library(INLA)
+
+# tidyverse packages- required for analysis
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(stringr)
+library(lubridate)
+
+# other
+suppressWarnings(suppressMessages(require(rstanarm)))
+
+### HELPER FUNCTIONS ###
