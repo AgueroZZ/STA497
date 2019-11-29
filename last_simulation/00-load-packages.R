2556f55cf332dcc2fffeb2be4ca23d05a044097f
AgueroZZ <49107284+AgueroZZ@users.noreply.github.com>
2019-11-29 16:24:53 -0500

    Last Simulation

---
+++ last_simulation/00-load-packages.R
@@ -0,0 +1,19 @@
+# LOAD LIBRARIES FOR CASE-CROSSOVER DATA ANALYSIS ###
+# ALSO INCLUDE HELPER FUNCTIONS
+libplace = "~/lib"
+# Packages required for computation
+library(parallel)
+library(Matrix)
+library(trustOptim,lib.loc = libplace)
+library(matrixStats) # for LogSumExp
+library(purrr)
+library(INLA,lib.loc = libplace)
+library(survival)
+
+# tidyverse packages- required for analysis
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(stringr)
+library(lubridate)
+
---
+++ last_simulation/install.R
@@ -0,0 +1,3 @@
+libplace = "~/lib"
+install.packages("trustOptim",lib.loc = libplace)
+install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE,lib = libplace)
