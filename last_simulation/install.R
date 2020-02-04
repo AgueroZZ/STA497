5166f528bbf61cd3d7ac32b0c4be81d4d3d9eaad
AgueroZZ <49107284+AgueroZZ@users.noreply.github.com>
2020-02-03 21:40:59 -0500

    instal_packages

--- last_simulation/install.R
+++ last_simulation/install.R
@@ -1,3 +1,4 @@
 libplace = "~/lib"
 install.packages("trustOptim",lib.loc = libplace)
 install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE,lib = libplace)
+install.packages("mvQuad",lib.loc = libplace)
